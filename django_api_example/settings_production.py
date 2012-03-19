87345ed8f8bccea05c19f72b1906d6ade88afb65
Andy Mroczkowski <andy@mrox.net>
2012-03-19 01:16:59 -0700

    comments

--- django_api_example/settings_production.py
+++ django_api_example/settings_production.py
@@ -3,7 +3,11 @@ import posixpath
 
 # database settings are injected by heroku automatically
 
+# Debug Media serving settings
+#
 # DON'T ACTUALLY SHIP WITH THESE SETTINGS
+# 
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 STATIC_URL = "/static/"
 ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
