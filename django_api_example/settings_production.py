1e18f6616587fa493e04666f967992b45cb4255b
Andy Mroczkowski <andy@mrox.net>
2012-03-19 01:05:09 -0700

    more media settings

--- django_api_example/settings_production.py
+++ django_api_example/settings_production.py
@@ -2,4 +2,9 @@ from settings_base import *
 
 # database settings are injected by heroku automatically
 
+# DON'T ACTUALLY SHIP WITH THESE SETTINGS
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
+STATIC_URL = "/static/"
+ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
+
 SERVE_MEDIA = DEBUG
