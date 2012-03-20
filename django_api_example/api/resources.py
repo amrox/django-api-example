0f21a0b2803ebfd174b5dd82d2792cf0c8a85a42
Andy Mroczkowski <andy@mrox.net>
2012-03-20 00:14:26 -0700

    adding DjangoAuthorization

--- django_api_example/api/resources.py
+++ django_api_example/api/resources.py
@@ -1,6 +1,7 @@
 from tastypie.resources import ModelResource
 from tastypie import fields
 from tastypie.authentication import OAuthAuthentication
+from tastypie.authorization import DjangoAuthorization
 
 from django.contrib.auth.models import User
 from tasks.models import Task
@@ -13,6 +14,7 @@ class UserResource(ModelResource):
         resource_name = 'users'
         excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
         authentication = OAuthAuthentication()
+        authorization = DjangoAuthorization()
 
     def apply_authorization_limits(self, request, object_list):
         return object_list.filter(self=request.user)
@@ -24,6 +26,7 @@ class TaskResource(ModelResource):
         queryset = Task.objects.all()
         resource_name = 'tasks'
         authentication = OAuthAuthentication()
+        authorization = DjangoAuthorization()
 
     def apply_authorization_limits(self, request, object_list):
         return object_list.filter(user=request.user)
