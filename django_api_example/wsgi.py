edab794bbafc531b0f82d6b6641a70d94dd52292
Andrew Mroczkowski <andy.mroczkowski@gmail.com>
2016-03-14 18:33:27 -0400

    Fixing settings location

--- django_api_example/wsgi.py
+++ django_api_example/wsgi.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-api-example.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_api_example.settings")
 
 import django.core.handlers.wsgi
 
