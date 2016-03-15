546f900aa1f8443e480f7d21136f758de7db49ae
Andrew Mroczkowski <andy.mroczkowski@gmail.com>
2016-03-15 12:40:04 -0400

    Because why not

--- django_api_example/settings_production.py
+++ django_api_example/settings_production.py
@@ -13,7 +13,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 STATIC_URL = "/static/"
 ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
 
-DEBUG = False
+DEBUG = True
 SERVE_MEDIA = DEBUG
 
 import dj_database_url
