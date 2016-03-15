6c68e43078bb5e858ddea84d44a943ec9d7808b4
Andrew Mroczkowski <andy.mroczkowski@gmail.com>
2016-03-15 13:00:34 -0400

    Turning off debug in production

--- django_api_example/settings_production.py
+++ django_api_example/settings_production.py
@@ -13,7 +13,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 STATIC_URL = "/static/"
 ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
 
-DEBUG = True
+DEBUG = False
 SERVE_MEDIA = DEBUG
 
 import dj_database_url
